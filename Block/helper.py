8dc3927cca0dbf92f3c0d98ae52ac948868c14d5
sshabihi <shabihish@gmail.com>
2021-08-25 18:18:17 +0430

    add_initial_block_functionality

---
+++ Block/helper.py
@@ -0,0 +1,7 @@
+from hashlib import sha3_256, sha384
+def gen_hash_encoded(data: str):
+    data_arr = bytearray(data, encoding='utf-8')
+    return sha384(sha3_256(data_arr).digest()).hexdigest()
+
+def gen_hash(data: bytearray):
+    return sha384(sha3_256(data).digest()).hexdigest()
---
+++ tests/test_block.py
@@ -0,0 +1,22 @@
+import pytest
+from crystaline.Block.Block import Block
+import time
+
+
+@pytest.fixture
+def mblock():
+    return Block('version', 'prev_hash', 1000, 999)
+
+
+def test_block_params(mblock):
+    assert mblock.version == 'version' and mblock.prev_hash == 'prev_hash' and mblock.difficulty_target == 1000 and mblock.nonce == 999
+
+
+def test_block_timestamp_is_early_enough(mblock):
+    print(mblock.timestamp)
+    assert mblock.timestamp <= time.time()
+
+
+def test_block_timestamp_is_exact(mblock):
+    print(mblock.timestamp)
+    assert mblock.timestamp == int(time.time())
