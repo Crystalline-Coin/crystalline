3f10cb1dacd2ab3985f7215adf803be964a10bf9
Alireza Arbabi <88896798+Alireza-Zwolf@users.noreply.github.com>
2021-09-01 09:59:42 +0430

    Update Blockchain.py

--- Blockchain/Blockchain.py
+++ Blockchain/Blockchain.py
@@ -2,8 +2,11 @@ import time
 import hashlib
 from crystaline.Block.Block import Block
 
-GENESIS_FIRST_BLOCK_DIFFICULTY = 0
+
 GENESIS_BLOCK_DIFFICULTY = 0
+NONCE = 0 
+ZERO = 0
+
 class Blockchain:
     
     def __init__(self):
@@ -12,9 +15,9 @@ class Blockchain:
 
     def add_new_block(self, difficulty_target):
         if(len(self.chain)):
-            new_block = Block(len(self.chain), self.last_block.generate_block_hash(), difficulty_target, GENESIS_BLOCK_DIFFICULTY , time.time())
+            new_block = Block(len(self.chain), self.last_block.generate_block_hash(), difficulty_target, NONCE , time.time())
         else:
-            new_block = Block(len(self.chain), GENESIS_FIRST_BLOCK_DIFFICULTY , difficulty_target, GENESIS_FIRST_BLOCK_DIFFICULTY , time.time())
+            new_block = Block(len(self.chain), ZERO , difficulty_target, GENESIS_BLOCK_DIFFICULTY , time.time())
         self.chain.append(new_block)
         return new_block
 
