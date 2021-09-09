fda3450fed119dd39b2fef8b2eb581a4aa5f96ab
ali ebrahimi <65724329+ae2079@users.noreply.github.com>
2021-09-09 22:39:08 +0430

    Add save_json and load_json method

--- transaction/Transactions.py
+++ transaction/Transactions.py
@@ -7,21 +7,26 @@ class Transaction:
         self.value = _value
         self.signature = _signature
 
-    def to_json(self):
+    def save_json(self):
         transactions_json = {
             "input_address" : self.input_address,
             "output_address": self.output_address,
             "value": self.value,
             "signature" : self.signature
         }
-        return json.dumps(transactions_json)
+        return transactions_json
 
-    def from_json(self, transactions_json):
-        loads_transactions_json = json.loads(transactions_json)
+    def to_json(self):
+        return json.dumps(self.save_json())
+
+    def load_json(self, loads_transactions_json):
         self.input_address = loads_transactions_json["input_address"]
         self.output_address = loads_transactions_json["output_address"]
         self.value = loads_transactions_json["value"]
         self.signature = loads_transactions_json["signature"]
+    
+    def from_json(self, transactions_json):
+        self.load_json(json.loads(transactions_json))
 
     def is_valid(self):
         pass
