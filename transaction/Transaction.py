61498676584a97f263169f46d977dc8bb341dd83
ali ebrahimi <65724329+ae2079@users.noreply.github.com>
2021-09-11 15:37:48 +0430

    Add save to file and load from file methods

---
+++ transaction/Transaction.py
@@ -0,0 +1,39 @@
+import json
+
+class Transaction: 
+    def __init__(self, _input_address, _output_address, _value, _signature):
+        self.input_address = _input_address
+        self.output_address = _output_address
+        self.value = _value
+        self.signature = _signature
+
+    def to_json(self):
+        transactions_json = {
+            "input_address" : self.input_address,
+            "output_address": self.output_address,
+            "value": self.value,
+            "signature" : self.signature
+        }
+        return json.dumps(transactions_json)
+
+    def from_json(self, transactions_json):
+        loads_transactions_json = json.loads(transactions_json)
+        self.input_address = loads_transactions_json["input_address"]
+        self.output_address = loads_transactions_json["output_address"]
+        self.value = loads_transactions_json["value"]
+        self.signature = loads_transactions_json["signature"]
+
+    def save(self, path):
+        file = open(path, "w")
+        json_string = self.to_json()
+        file.write(json_string)
+        file.close()
+
+    def load(self, path):
+        file = open(path, "r")
+        json_string = file.read()
+        self.from_json(json_string)
+        file.close()
+
+    def is_valid(self):
+        pass
