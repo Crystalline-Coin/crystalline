79ad254b0d88de3f0c04d322493348f120517d6d
Alireza Arbabi <88896798+Alireza-Zwolf@users.noreply.github.com>
2021-09-19 15:09:55 +0430

    Update fee_calculator.py

--- fee_calculator/fee_calculator.py
+++ fee_calculator/fee_calculator.py
@@ -9,22 +9,6 @@ N = 1
 BETA = 20
 
 
-def calculate_fee(time_period , uploaded_file_size , uploader_address):
-    F_x = F_x_calculator(uploaded_file_size)
-    time_period = time_period * 24 * 60 * 60
-    G_x = 1
-    for i in range(len(Blockchain.chain) , 0 ,-1):
-        now_time = int(time.time())
-        times_diffrence  = now_time - Blockchain.chain[i].timestamp
-        if(times_diffrence > time_period):
-            break
-        else:
-            for j in range(0 , len(Blockchain.chain[i].files)):                      
-                if Blockchain.chain[i].files[j].creator() == uploader_address:
-                    G_x *= G_x_calculator(times_diffrence , uploaded_file_size)
-    fee = F_x * G_x
-    return fee
-
 def F_x_calculator(alpha , uploaded_file_size):                                  # uploaded_file_size : S_c
     F_x = uploaded_file_size * alpha + alpha        
     for i in range(0 , int(uploaded_file_size)):                                 # applying floors
