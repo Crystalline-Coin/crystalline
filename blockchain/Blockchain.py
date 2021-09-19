edffd61a41b7de9991e2a8493bf6cb91a028fc28
Alireza Arbabi <88896798+Alireza-Zwolf@users.noreply.github.com>
2021-09-19 15:13:11 +0430

    Update Blockchain.py

--- blockchain/Blockchain.py
+++ blockchain/Blockchain.py
@@ -1,6 +1,7 @@
 import time
 import hashlib
 from crystaline.block.Block import Block
+from crystaline.fee_calculator import fee_calculator
 
 GENESIS_FIRST_BLOCK_DIFFICULTY = 0
 GENESIS_BLOCK_DIFFICULTY = 0
@@ -28,6 +29,22 @@ class Blockchain:
             if (previous_block.timestamp > current_block.timestamp):
                 return False
         return True
+    
+        def calculate_fee(self , time_period , uploaded_file_size , uploader_address):
+            F_x = F_x_calculator(uploaded_file_size)
+            time_period = time_period * 24 * 60 * 60
+            G_x = 1
+            for i in range(len(self.chain) , 0 ,-1):
+                now_time = int(time.time())
+                times_diffrence  = now_time - self.chain[i].timestamp
+                if(times_diffrence > time_period):
+                    break
+                else:
+                    for j in range(0 , len(self.chain[i].files)):                      
+                        if self.chain[i].files[j] == uploader_address:
+                            G_x *= G_x_calculator(times_diffrence , uploaded_file_size)
+            fee = F_x * G_x
+            return fee
     
     @property
     def last_block(self):
