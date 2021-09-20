f6c397a1035f5d92e3d4f20c6477c7976b1814d7
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-09-21 00:49:25 +0430

    add signature and verification functions

--- transaction/Signature.py
+++ transaction/Signature.py
@@ -1,7 +1,32 @@
 import Transaction as tr
 import hashlib as hl
+from ecpy.ecdsa import ECDSA
+from ecpy.curves import Curve, Point
+from ecpy.keys import ECPublicKey, ECPrivateKey
+
+CURVE_NAME = 'Ed448'
 
 def sign(transaction, privatekey):
+    
     transaction_details = transaction.input_address + transaction.output_address + transaction.value
-    z = hl.sha256(transaction_details.encode()).hexdigest()
-    return z
+    trans_hash = hl.sha256(transaction_details.encode()).digest()
+    
+    curve = Curve.get_curve(CURVE_NAME)
+    private_key = ECPrivateKey(privatekey, curve)
+    
+    signer = ECDSA()
+    signature = signer.sign(trans_hash, private_key)
+    
+    return signature
+
+def verify_signature(transaction, publickey):
+    
+    transaction_details = transaction.input_address + transaction.output_address + transaction.value
+    trans_hash = hl.sha256(transaction_details.encode()).digest()
+
+    public_key = ECPublicKey(publickey)
+
+    signer = ECDSA()
+    signature = transaction.signature
+    
+    return signer.verify(trans_hash ,signature ,public_key)
