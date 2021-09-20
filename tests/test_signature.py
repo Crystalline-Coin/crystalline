4112557a3ddf46c3309a1d5327a1adbb9939d76b
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-09-21 01:05:38 +0430

    add test_signature

---
+++ tests/test_signature.py
@@ -0,0 +1,7 @@
+import pytest
+import os
+import sys
+sys.path.append('./public_address/')
+import public_address 
+from public_address import public_address_generator
+from public_address.public_address_generator import PublicAddressGenerator
