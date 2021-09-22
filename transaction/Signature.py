09818528c16306296925f346e38851fd6f87ce65
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-09-22 17:26:25 +0330

    Update Signature hash

--- transaction/Signature.py
+++ transaction/Signature.py
@@ -1,4 +1,3 @@
-import hashlib as hl
 from ecpy.ecdsa import ECDSA
 from ecpy.curves import Curve, Point
 from ecpy.keys import ECPublicKey, ECPrivateKey
@@ -6,30 +5,22 @@ from ecpy.keys import ECPublicKey, ECPrivateKey
 CURVE_NAME = 'Ed448'
 
 def sign(transaction, privatekey):
-    
-    transaction_details = transaction.get_details()
-    trans_hash = hl.sha256(
-                        transaction_details.encode()
-                    ).digest()
+    trans_hash = transaction.get_hash()
     
     curve = Curve.get_curve(CURVE_NAME)
     private_key = ECPrivateKey(privatekey, curve)
     
     signer = ECDSA()
-    signature = signer.sign(trans_hash, private_key)
+    signature = signer.sign(bytearray(trans_hash, encoding='utf-8'), private_key)
     
     return signature
 
 def verify_signature(transaction, publickey):
-    
-    transaction_details = transaction.get_details()
-    trans_hash = hl.sha256(
-                        transaction_details.encode()
-                    ).digest()
+    trans_hash = transaction.get_hash()
 
     public_key = ECPublicKey(publickey)
 
     signer = ECDSA()
     signature = transaction.signature
     
-    return signer.verify(trans_hash ,signature ,public_key)
+    return signer.verify(bytearray(trans_hash, encoding='utf-8') ,signature ,public_key)
