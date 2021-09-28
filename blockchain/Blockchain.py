7f63652cf1ce87fafb3634072f182e7a6b2e20f4
Alireza Arbabi <88896798+Alireza-Zwolf@users.noreply.github.com>
2021-09-28 18:46:55 +0330

    Fix indentation problem
    Fix false indentation in function ((calculate fee))

--- blockchain/Blockchain.py
+++ blockchain/Blockchain.py
@@ -32,21 +32,21 @@ class Blockchain:
                 return False
         return True
     
-        def calculate_fee(self , time_period , uploaded_file_size , uploader_address):
-            F_x = F_x_calculator(uploaded_file_size)
-            time_period = time_period * 24 * 60 * 60
-            G_x = 1
-            for i in range(len(self.chain) , 0 ,-1):
-                now_time = int(time.time())
-                times_diffrence  = now_time - self.chain[i].timestamp
-                if(times_diffrence > time_period):
-                    break
-                else:
-                    for j in range(0 , len(self.chain[i].files)):                      
-                        if self.chain[i].files[j] == uploader_address:
-                            G_x *= G_x_calculator(times_diffrence , uploaded_file_size)
-            fee = F_x * G_x
-            return fee
+    def calculate_fee(self , time_period , uploaded_file_size , uploader_address):
+        F_x = F_x_calculator(uploaded_file_size)
+        time_period = time_period * 24 * 60 * 60
+        G_x = 1
+        for i in range(len(self.chain) , 0 ,-1):
+        now_time = int(time.time())
+        times_diffrence  = now_time - self.chain[i].timestamp
+        if(times_diffrence > time_period):
+            break
+        else:
+            for j in range(0 , len(self.chain[i].files)):                      
+                if self.chain[i].files[j] == uploader_address:
+        G_x *= G_x_calculator(times_diffrence , uploaded_file_size)
+        fee = F_x * G_x
+        return fee
     
     @property
     def last_block(self):
