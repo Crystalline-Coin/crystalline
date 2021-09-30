023abc24c6d776f881b8cdd27b23bc8d99c0b2f7
tahafkh <fakharian.taha@ut.ac.ir>
2021-09-30 23:24:19 +0330

    Update test

--- tests/test_net.py
+++ tests/test_net.py
@@ -4,13 +4,14 @@ import pytest
 
 from crystaline.net.node import Node
 from crystaline.net.node import DEFAULT_PROTOCOL, DEFAULT_METHODS
-from crystaline.net.node import URL_ADD_NODE, URL_GET_NODES, URL_GET_STATUS, URL_GET_BLOCK
+from crystaline.net.node import URL_ADD_NODE, URL_GET_NODES, URL_GET_STATUS, URL_GET_BLOCK, URL_ADD_TXO, URL_ADD_FILE
 from crystaline.net.node import PARAM_IP, PARAM_PORT, PARAM_BLOCK_INDEX, PARAM_NODES_LIST_STATUS, PARAM_NODES_LIST_PORT
 from crystaline.net.node import STATUS_RADDR_UP
 from crystaline.net.node import get_peer_status
 
 from crystaline.blockchain.Blockchain import Blockchain
 
+import json
 import requests
 
 NODE_IP_ADDR = '127.0.0.1'
@@ -132,6 +133,62 @@ def test_net_get_block_state_200(running_node_with_blockchain):
     elif 'GET' in DEFAULT_METHODS:
         res = requests.get(req_get_block_url,
                            params={PARAM_BLOCK_INDEX: 1})
+    else:
+        raise Exception('No consistent RESTful methods used.')
+
+    assert res.status_code == 200
+    res.close()
+
+def test_net_add_file_state_200(running_node_with_blockchain):
+    wait_for_url(url=DEFAULT_PROTOCOL + '://' + NODE_IP_ADDR + ':' + str(NODE_PORT) + URL_GET_STATUS,
+                 method=DEFAULT_METHODS[0],
+                 timeout=2)
+    req_add_file_url = DEFAULT_PROTOCOL + '://' + NODE_IP_ADDR + ':' + str(NODE_PORT) + URL_ADD_FILE
+    sample_file = {
+        '_content' : 'content',
+        '_name' : 'file_name',
+        '_creator' : 'file_creator',
+        '_creation_transaction' : 'transaction'
+    }
+
+    if 'POST' in DEFAULT_METHODS:
+        res = requests.post(req_add_file_url,
+                            json= json.dumps(sample_file),
+                            headers = {'ContentType': 'application/json'})
+    elif 'GET' in DEFAULT_METHODS:
+        res = requests.get(req_add_file_url,
+                            json= json.dumps(sample_file),
+                           headers = {'ContentType': 'application/json'})
+    else:
+        raise Exception('No consistent RESTful methods used.')
+
+    assert res.status_code == 200
+    res.close()
+
+
+def test_net_add_txo_state_200(running_node_with_blockchain):
+    wait_for_url(url=DEFAULT_PROTOCOL + '://' + NODE_IP_ADDR + ':' + str(NODE_PORT) + URL_GET_STATUS,
+                 method=DEFAULT_METHODS[0],
+                 timeout=2)
+    req_add_file_url = DEFAULT_PROTOCOL + '://' + NODE_IP_ADDR + ':' + str(NODE_PORT) + URL_ADD_TXO
+    sample_txo = {
+        'input_address': {
+            '1' : '2'
+        },
+        'output_address': {
+            '1': '2'
+        },
+        'signature': 'signature'
+    }
+
+    if 'POST' in DEFAULT_METHODS:
+        res = requests.post(req_add_file_url,
+                            json=json.dumps(sample_txo),
+                            headers = {'ContentType': 'application/json'})
+    elif 'GET' in DEFAULT_METHODS:
+        res = requests.get(req_add_file_url,
+                            json=json.dumps(sample_txo),
+                           headers = {'ContentType': 'application/json'})
     else:
         raise Exception('No consistent RESTful methods used.')
 
