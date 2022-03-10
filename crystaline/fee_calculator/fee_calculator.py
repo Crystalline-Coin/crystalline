f514c8c09792ce05a0cd40d12716cdaf29701d59
ae2079 <aliebrahimi2079@gmail.com>
2022-03-11 01:14:29 +0330

    Write fee calculator test

--- crystaline/fee_calculator/fee_calculator.py
+++ crystaline/fee_calculator/fee_calculator.py
@@ -1,32 +1,29 @@
-import time
-from ..blockchain import Blockchain
-
-
-
-TIME_PERIOD = 30                                                                  #time_period : t
+TIME_PERIOD = 30                                                                # time_period : t
 ALPHA = 2
 N = 1
 BETA = 20
 
 
-def F_x_calculator(alpha , uploaded_file_size):                                  # uploaded_file_size : S_c
+def F_x_calculator(alpha, uploaded_file_size):                                  # uploaded_file_size : S_c
     F_x = uploaded_file_size * alpha + alpha        
-    for i in range(0 , int(uploaded_file_size)):                                 # applying floors
+    for i in range(0, int(uploaded_file_size)):                                 # applying floors
         F_x += 1
     return F_x
 
-def H_x_calculator(time_period , n , times_diffrence):                                   
+
+def H_x_calculator(time_period, n, times_difference):
     time_period = time_period * 24 * 60 * 60
-    H_x = 0
-    if(times_diffrence <= time_period):
-        H_x = pow(time_period/times_diffrence , n)
+    if times_difference <= time_period:
+        H_x = pow(time_period/times_difference, n)
     else:
         H_x = 1
     return H_x
 
-def K_x_calculator(time_period , beta , file_size):                               #file size: s
+
+def K_x_calculator(time_period, beta, file_size):                               # file size: s
     k_x = -(time_period/(file_size+(time_period/beta))) + beta
     return k_x
 
-def G_x_calculator(times_diffrence , file_size):
-    return K_x_calculator(file_size) * H_x_calculator(times_diffrence)
+
+def G_x_calculator(time_period, times_difference, file_size):
+    return K_x_calculator(time_period, BETA, file_size) * H_x_calculator(time_period, N, times_difference)
---
+++ crystaline/tests/test_fee_calculator.py
@@ -0,0 +1,24 @@
+import pytest
+from ..fee_calculator import fee_calculator as fc
+
+SIZE = 123
+TIME_1 = 10
+TIME_2 = 20
+N = 4
+
+
+def test_F_x():
+    assert fc.F_x_calculator(fc.ALPHA, SIZE) == 371
+
+
+def test_H_x():
+    assert fc.H_x_calculator(TIME_1, N, TIME_2) == 3.4828517376e+18
+    assert fc.H_x_calculator(TIME_2, N, TIME_1) == 8.916100448256e+20
+
+
+def test_K_x():
+    assert fc.K_x_calculator(TIME_1, fc.BETA, SIZE) == 19.91902834008097
+
+
+def test_G_x():
+    assert fc.G_x_calculator(TIME_1, TIME_2, SIZE) == 860502.0242914979
