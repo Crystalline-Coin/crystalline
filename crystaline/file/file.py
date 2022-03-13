3ffe5ebbd02b61a1289ebfda9c244abc2ce82b6d
sshabihi <shabihish@gmail.com>
2022-03-13 12:33:15 +0330

    Update file tests

--- crystaline/file/file.py
+++ crystaline/file/file.py
@@ -1,9 +1,12 @@
 import json
 from ..block import helper as hp
 
+
 class File:
-    PARAM_NAME, PARAM_CONTENT, PARAM_CREATOR, PARAM_CREATION_TXO = '_name', '_content', '_creator', '_creation_transaction'
-    def __init__(self, content, name ,  creator = None, creation_transaction = None):
+    PARAM_NAME, PARAM_CONTENT, PARAM_CREATOR, PARAM_CREATION_TXO = \
+        '_name', '_content', '_creator', '_creation_transaction'
+
+    def __init__(self, content, name, creator=None, creation_transaction=None):
         self._content = content
         self._name = name
         self._creator = creator
@@ -34,26 +37,26 @@ class File:
         return self.__dict__
 
     @classmethod
-    def from_dict(cls, dict):
-        return cls(dict['_content'], dict['_name'],
-                    dict['_creator'], dict['_creation_transaction'])
+    def from_dict(cls, dict_in):
+        return cls(dict_in[cls.PARAM_CONTENT], dict_in[cls.PARAM_NAME],
+                   dict_in[cls.PARAM_CREATOR], dict_in[cls.PARAM_CREATION_TXO])
 
     def to_json(self):
         return json.dumps(self.__dict__)
 
-
     def get_hash(self):
-        return hp.gen_hash(str(self.to_json()))
+        return hp.gen_hash(self.to_json())
 
     @staticmethod
     def from_json(file_json):
-        return json.loads(file_json, object_hook = lambda obj:
-                                    File(obj['_content'], obj['_name'],
-                                    obj['_creator'], obj['_creation_transaction']))
+        return json.loads(file_json, object_hook=lambda obj:
+        File(obj['_content'], obj['_name'],
+             obj['_creator'], obj['_creation_transaction']))
 
     def __eq__(self, other):
         if isinstance(self, other.__class__):
             return other.content == self.content and other._name == self._name \
-            and other._creator == self._creator and other._creation_transaction == self._creation_transaction \
-            and  other._file_hash == self._file_hash
+                   and other._creator == self._creator and other._creation_transaction == self._creation_transaction \
+                   and other._file_hash == self._file_hash
         return False
+
--- crystaline/tests/test_file.py
+++ crystaline/tests/test_file.py
@@ -9,15 +9,21 @@ F_CONTENT = 'f_content'
 F_CREATOR = 'f_creator'
 F_CREATION_TXO = 'f_creation_txo'
 
+
 @pytest.fixture
-def mfile():
-    cls_dict = {
+def file_dict():
+    return {
         File.PARAM_NAME: F_NAME,
         File.PARAM_CONTENT: F_CONTENT,
         File.PARAM_CREATOR: F_CREATOR,
         File.PARAM_CREATION_TXO: F_CREATION_TXO,
     }
-    return File.from_dict(cls_dict)
+
+
+@pytest.fixture
+def mfile(file_dict: dict):
+    return File.from_dict(file_dict)
+
 
 def test_file_properties(mfile: File):
     assert mfile.name == F_NAME and \
@@ -25,11 +31,19 @@ def test_file_properties(mfile: File):
            mfile.creator == F_CREATOR and \
            mfile.creation_transaction == F_CREATION_TXO
 
-def test_file_json(mfile: File):
+
+def test_file_to_json(mfile: File):
     f_dict = json.loads(mfile.to_json())
     assert f_dict[File.PARAM_NAME] == F_NAME and \
            f_dict[File.PARAM_CONTENT] == F_CONTENT and \
            f_dict[File.PARAM_CREATOR] == F_CREATOR and \
            f_dict[File.PARAM_CREATION_TXO] == F_CREATION_TXO
+
+
+def test_file_from_json(mfile: File, file_dict: dict):
+    f_json = json.dumps(file_dict)
+    assert File.from_json(f_json) == mfile
+
+
 def test_files_equality(mfile):
     assert mfile == deepcopy(mfile)
