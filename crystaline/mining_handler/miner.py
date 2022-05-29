9c3241d24fd6f55885a20e14f1cc4322503103e0
sinku <sininoir@gmail.com>
2022-05-29 13:49:22 +0430

    Fix bugs

--- crystaline/mining_handler/miner.py
+++ crystaline/mining_handler/miner.py
@@ -7,7 +7,7 @@ import time
 
 
 class Miner:
-    def __init__(self, blockchain:Blockchain, file_pool, transaction_pool) -> None:
+    def __init__(self, blockchain:Blockchain, file_pool:list, transaction_pool:dict) -> None:
         self.blockchain = blockchain
         self.file_pool = file_pool
         self.transaction_pool = transaction_pool
@@ -27,7 +27,7 @@ class Miner:
     
     def mine_block(self):
         files_to_be_mined = self.get_items_to_be_mined(self.file_pool, BLOCK_FILE_SIZE)
-        transactions_to_be_mined = self.get_items_to_be_mined(self.transaction_pool, BLOCK_TRANSACTION_SIZE)
+        transactions_to_be_mined = self.get_items_to_be_mined(self.transaction_pool.values(), BLOCK_TRANSACTION_SIZE)
 
         for i in range(NONCE_RANGE[0], NONCE_RANGE[1]):
             block = Block(
@@ -42,6 +42,6 @@ class Miner:
             if block.is_valid():
                 self.blockchain.add_block(block)
                 self.file_pool = [x for x in self.file_pool if x not in files_to_be_mined]
-                self.transaction_pool = [x for x in self.transaction_pool if x not in transactions_to_be_mined]
+                self.transaction_pool = {k: v for k, v in self.transaction_pool.items() if v not in transactions_to_be_mined}
                 return block
         return None
