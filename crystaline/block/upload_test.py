d5a58ddb515bb0a5f4fbebad6dffc790052ef148
tahafkh <fakharian.taha@ut.ac.ir>
2022-07-22 22:38:31 +0430

    Delete unnecessary tests

--- crystaline/block/upload_test.py
+++
@@ -1,9 +0,0 @@
-from crystaline.block.block import Block
-
-FILE_PATH = "/home/taha/Downloads/michael-jackson-billie-jean-128.mp3"
-NEW_FILE_PATH = "/home/taha/Desktop"
-
-if __name__ == "__main__":
-    new_block = Block("00", "000000", 1, 1)
-    new_block.upload_file(FILE_PATH)
-    new_block.download_file(NEW_FILE_PATH, 0)
